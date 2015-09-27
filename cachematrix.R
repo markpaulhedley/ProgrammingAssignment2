9273163d66b02fa7cfd65a44c298de4ea9747b87
markpaulhedley <markpaulhedley@gmail.com>
2015-09-27 21:12:26 +0200

    Programming assignment 2
    my attempt at programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this file contains two functions to fulfill the requirements 
+## of R programing- assignment 2. The two functions are makeCacheMatrix
+## whichcreates a matrix that can cache its own inverse and
+## cachesolve which will either look up an already "solved"
+## matrix or find the inverse depending on weather this action 
+## has been completed already.
 
-## Write a short comment describing this function
+## This is the first function which creates a matrix object and
+## can cache its own inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {                            #this sets the matrix's vaue
+    x <<- y
+    m <<- NULL
+  }
+  get <- function()                               #this retrives the value of the matrix
+  setinverse <- function(inv) m <<- inv           #this sets the inverse value
+  getinverse <- function() m                      #this gets the inverse value
+  list(set = set, get = get,
+       setinverse = setinverse,                   #list of thefour functions that are defined above
+       getinverse = getinverse)
+  
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {        ## Return a matrix that is the inverse of 'x'
+        
+  m <- x$getinverse()                   ##looks in the cache
+  if(!is.null(m)) {                     ##asks if the cache has something in it
+    message("getting cached data")      
+    return(m)                           ##and returns the stored data if its there
+  }
+  data <- x$get()
+  m <- solve(data, ...)                 ##otherwise the inverse is computed stored in the cache and the result returned
+  x$setinverse(m)
+  m
+  
 }
